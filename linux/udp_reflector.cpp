aab593212434c017df881bf7e93e8927749a18c7
Mateusz Czapliński <mateusz.czaplinski@zpas-lab.pl>
2018-11-30 12:30:59 +0100

    add helpful info in -s error message

--- linux/udp_reflector.cpp
+++ linux/udp_reflector.cpp
@@ -411,7 +411,7 @@ int main(int argc, char *argv[])
                 {
                     if (strncmp(pcap_dev, "pcap", 4) != 0)
                     {
-                        fprintf(stderr, "Invalid source pcap interface");
+                        fprintf(stderr, "Invalid source pcap interface (see -l for a list of available interfaces)");
                         exit(1);
                     }
 
